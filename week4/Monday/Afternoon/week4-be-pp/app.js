a454fa1da8aac3fed624e3ebcc2c95edf41b5491
Yicheng CHEN <yicheng4499@gmail.com>
2025-09-09 00:17:56 +0300

    finish week4 monday morning activity1

--- week4/Monday/Afternoon/week4-be-pp/app.js
+++ week4/Monday/Afternoon/week4-be-pp/app.js
@@ -1,11 +1,20 @@
 const express = require("express");
 const app = express();
 const tourRouter = require("./routes/tourRouter");
+const userRouter = require('./routes/userRouter');
+
+const morgan = require('morgan');
+
+const authorizeUsersAccess = require('./middleware/auth');
 
 // Middleware to parse JSON
+app.use(morgan('tiny'));
+
 app.use(express.json());
 
 app.use("/tours", tourRouter);
+
+app.use('/users', authorizeUsersAccess, userRouter);
 
 const port = 4000;
 // Start the server
