e7dc0caf059419ef31b186d1326d4aef570e3370
Yicheng CHEN <yicheng4499@gmail.com>
2025-09-14 17:45:08 +0300

    update userControllers.js

--- week4/Wednesday/Morning/week4-be-activity1/controllers/userControllers.js
+++ week4/Wednesday/Morning/week4-be-activity1/controllers/userControllers.js
@@ -2,73 +2,53 @@ const User = require("../models/userModel");
 
 // GET /users
 const getAllUsers = async (req, res) => {
-  try {
-    const users = await User.find({}).sort({ createdAt: -1 });
-    res.status(200).json(users);
-  } catch (error) {
-    res.status(500).json({ message: error.message });
-  }
+  const users = await User.find({}).sort({ createdAt: -1 });
+  res.status(200).json(users);
 };
 
 // POST /users
 const createUser = async (req, res) => {
-  try {
-    const newUser = await User.create({ ...req.body });
-    res.status(201).json(newUser);
-  } catch (error) {
-    res.status(400).json({ message: error.message });
-  }
+  const newUser = await User.create({ ...req.body });
+  res.status(201).json(newUser);
 };
 
 // GET /users/:userId
 const getUserById = async (req, res) => {
-  try {
-    const { userId } = req.params;
-    const user = await User.findById(userId);
-    
-    if (user) {
-      res.status(200).json(user);
-    } else {
-      res.status(404).json({ message: "User not found" });
-    }
-  } catch (error) {
-    res.status(500).json({ message: error.message });
+  const { userId } = req.params;
+  const user = await User.findById(userId);
+
+  if (user) {
+    res.status(200).json(user);
+  } else {
+    res.status(404).json({ message: "User not found" });
   }
 };
 
 // PUT /users/:userId
 const updateUser = async (req, res) => {
-  try {
-    const { userId } = req.params;
-    const updatedUser = await User.findOneAndUpdate(
-      { _id: userId },
-      { ...req.body },
-      { new: true, runValidators: true } // 添加 runValidators 确保更新时也验证数据
-    );
-    
-    if (updatedUser) {
-      res.status(200).json(updatedUser);
-    } else {
-      res.status(404).json({ message: "User not found" });
-    }
-  } catch (error) {
-    res.status(400).json({ message: error.message });
+  const { userId } = req.params;
+  const updatedUser = await User.findOneAndUpdate(
+    { _id: userId },
+    { ...req.body },
+    { new: true, runValidators: true } // 添加 runValidators 确保更新时也验证数据
+  );
+
+  if (updatedUser) {
+    res.status(200).json(updatedUser);
+  } else {
+    res.status(404).json({ message: "User not found" });
   }
 };
 
 // DELETE /users/:userId
 const deleteUser = async (req, res) => {
-  try {
-    const { userId } = req.params;
-    const deletedUser = await User.findOneAndDelete({ _id: userId });
-    
-    if (deletedUser) {
-      res.status(200).json({ message: "User deleted successfully" });
-    } else {
-      res.status(404).json({ message: "User not found" });
-    }
-  } catch (error) {
-    res.status(500).json({ message: error.message });
+  const { userId } = req.params;
+  const deletedUser = await User.findOneAndDelete({ _id: userId });
+
+  if (deletedUser) {
+    res.status(200).json({ message: "User deleted successfully" });
+  } else {
+    res.status(404).json({ message: "User not found" });
   }
 };
 
